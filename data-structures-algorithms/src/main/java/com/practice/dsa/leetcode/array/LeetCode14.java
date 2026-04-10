e7d78e9805596923e24b108fd314c43f1230c18a
XIAO SHI LIN <aquarius.c.c.a@gmail.com>
2026-04-10 16:31:25 +0800

    LeetCode 14

--- data-structures-algorithms/src/main/java/com/practice/dsa/leetcode/array/LeetCode14.java
+++ data-structures-algorithms/src/main/java/com/practice/dsa/leetcode/array/LeetCode14.java
@@ -3,38 +3,52 @@ package com.practice.dsa.leetcode.array;
 // 題目：https://leetcode.cn/problems/longest-common-prefix/?envType=problem-list-v2&envId=array
 public class LeetCode14 {
     public static void main(String[] args) {
-        String[] strs1 = { "flower","flow","flight" };
-        System.out.println(longestCommonPrefix(strs1));
+        String[] strs1 = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(strs1)); // fl
 
-        String[] strs2 = { "a" };
-        System.out.println(longestCommonPrefix(strs2));
+        String[] strs2 = {"a"};
+        System.out.println(longestCommonPrefix(strs2)); // a
+
+        String[] strs3 = {"dog", "racecar", "car"};
+        System.out.println(longestCommonPrefix(strs3)); // ""
     }
 
     public static String longestCommonPrefix(String[] strs) {
+        // 邊界處理：
+        // 如果陣列是 null，或陣列長度為 0，直接回傳空字串
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        // 先假設第一個字串是最短字串
         String minStr = strs[0];
+
+        // 找出陣列中最短的字串
+        // 因為最長公共前綴的長度，不可能超過最短字串的長度
         for (int i = 1; i < strs.length; i++) {
-            if (minStr.length() > strs[i].length()) {
+            if (strs[i].length() < minStr.length()) {
                 minStr = strs[i];
             }
         }
-        int i = 0;
-        String res = "";
-        while (i < minStr.length()) {
+
+        // 逐個字元檢查最短字串的每一個位置
+        for (int i = 0; i < minStr.length(); i++) {
+            // 取出最短字串當前位置的字元，作為比較基準
             char c = minStr.charAt(i);
-            boolean flag = true;
+
+            // 拿這個字元去和所有字串同一位置的字元比較
             for (int j = 0; j < strs.length; j++) {
-                if (c != strs[j].charAt(i)) {
-                    flag = false;
-                    break;
+                // 只要有任何一個字串在這個位置的字元不同，
+                // 代表公共前綴到 i 之前就結束了
+                if (strs[j].charAt(i) != c) {
+                    // 回傳從 0 到 i-1 的子字串
+                    return minStr.substring(0, i);
                 }
             }
-            if (flag){
-                res += c;
-                i++;
-            } else {
-                break;
-            }
         }
-        return res;
+
+        // 如果整個最短字串都比完了，仍然都相同，
+        // 代表最短字串本身就是最長公共前綴
+        return minStr;
     }
 }
